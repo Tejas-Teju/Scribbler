07ef95f6e9b67acc12c50976363d71136c150072
Tejas-Teju <tejas8196@gmail.com>
2021-03-07 18:10:31 +0530

    Feat & Style: Added Edit-Save functionality - Button toggles between edit and save - Margin added to author and button instead of post title  - Changed heading font size to x-larger

--- scripts/post.js
+++ scripts/post.js
@@ -10,3 +10,29 @@ fetch("../index.html")
         var clone = document.importNode(template.content, true);
         document.getElementById('header').appendChild(clone);
   });
+
+ document.getElementsByClassName('btn-edit')[0].addEventListener('click', function() {
+      document.getElementsByClassName('btn-edit')[0].style.display = 'none';
+      document.getElementsByClassName('btn-save')[0].style.display = 'initial';
+
+      var postTitle = document.getElementById('post-title');
+      postTitle.setAttribute('contenteditable', 'true');
+      postTitle.style.border = '1px solid #fa3275';
+
+      var postTitle = document.getElementsByClassName('post-content')[0];
+      postTitle.setAttribute('contenteditable', 'true');
+      postTitle.style.border = '1px solid #fa3275';
+  });
+
+ document.getElementsByClassName('btn-save')[0].addEventListener('click', function() {
+    document.getElementsByClassName('btn-save')[0].style.display = 'none';
+    document.getElementsByClassName('btn-edit')[0].style.display = 'initial';
+
+    var postTitle = document.getElementById('post-title');
+    postTitle.setAttribute('contenteditable', 'false');
+    postTitle.style.border = 'none';
+
+    var postTitle = document.getElementsByClassName('post-content')[0];
+    postTitle.setAttribute('contenteditable', 'false');
+    postTitle.style.border = 'none';
+});
